f42e06b54841319043fab04b8f9a33ad7026cb12
David Bridgwood <dbridg15@gmail.com>
2018-01-13 18:28:02 +0000

    more model fitting

--- CMEEMiniProject/Code/sort_data.py
+++ CMEEMiniProject/Code/sort_data.py
@@ -2,15 +2,19 @@ import pandas as pd
 
 data = pd.read_csv("../Data/GrowthRespPhotoData_new.csv", low_memory = False)
 
+
+# get rid of 0s -ves and NAs from data (only +ves)
+data2 = data.iloc[data.index[data["StandardisedTraitValue"] > 0]]
+
+
 # get rid of ids with < 5 data points
-data2 = data.groupby("OriginalID").filter(lambda x: len(x) > 5)
+data2 = data2.groupby("FinalID").filter(lambda x: len(x) > 5)
 
 # set NewID based on OriginalID **do i even need to do this?**
-data2["NewID"] = data2.OriginalID.astype("category").cat.codes
+#data2["NewID"] = data2.OriginalID.astype("category").cat.codes
 
 # only columns i need
-data3 = data2.loc[ : ,("OriginalID",
-                       "NewID",
+data2 = data2.loc[ : ,("FinalID",
                        "OriginalTraitName",
                        "OriginalTraitDef",
                        "OriginalTraitValue",
@@ -24,12 +28,8 @@ data3 = data2.loc[ : ,("OriginalID",
                        "ConTemp",
                        "ConTempUnit",
                        "ResTemp",
-                       "ResTempUnit")]
-
-# getting list of indexes for trait values less then 0. Not sure what to do with
-# them though...
-a = data2.index[data2["StandardisedTraitValue"] < 0].tolist()
-data2.StandardisedTraitValue.loc[a]
+                       "ResTempUnit",
+                       "Consumer")]
 
 
 # save the new df
