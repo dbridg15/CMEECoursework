3d1f72c7d931bd48dd9f549886cb6f430d3ff22c
David Bridgwood <dmb2417@ic.ac.uk>
2018-02-28 18:04:30 +0000

    compare!

--- CMEEMiniProject/Code/compare.R
+++ CMEEMiniProject/Code/compare.R
@@ -14,113 +14,107 @@ require(tidyr)
 # read in data
 ###############################################################################
 
-cubicDF <- read.csv("../Results/cubic_model.csv")
-flschDF <- read.csv("../Results/full_scholfield_model.csv")
-nhschDF <- read.csv("../Results/noh_scholfield_model.csv")
-nlschDF <- read.csv("../Results/nol_scholfield_model.csv")
-arrhnDF <- read.csv("../Results/arrhenius_model.csv")
+cubicDF = read.csv("../Results/cubic_model.csv")
+flschDF = read.csv("../Results/full_scholfield_model.csv")
+nhschDF = read.csv("../Results/noh_scholfield_model.csv")
+nlschDF = read.csv("../Results/nol_scholfield_model.csv")
+arrhnDF = read.csv("../Results//arrhenius_model.csv")
 
-aicdf <- data.frame("NewID" = cubicDF$NewID,
-                    "cubic" = cubicDF$aic,
-                    "flsch" = flschDF$aic,
-                    "nhsch" = nhschDF$aic,
-                    "nlsch" = nlschDF$aic)
+aicdf = data.frame("NewID" = cubicDF$NewID,
+                  "cubic" = cubicDF$nlaic,
+                  "flsch" = flschDF$nlaic,
+                  "nhsch" = nhschDF$nlaic,
+                  "nlsch" = nlschDF$nlaic,
+                  "arrhn" = arrhnDF$nlaic)
 
 ###############################################################################
-# calculate aic delta and weight for each model
+# functions
 ###############################################################################
 
-cubic_delta <- function(row){
-  abs(min(row, na.rm = TRUE) - row["cubic"])
+
+delta <- function(row, model){
+  abs(min(row, na.rm = TRUE) - row[model])
 }
 
-flsch_delta <- function(row){
-  abs(min(row, na.rm = TRUE) - row["flsch"])
-}
-
-nhsch_delta <- function(row){
-  abs(min(row, na.rm = TRUE) - row["nhsch"])
-}
-
-nlsch_delta <- function(row){
-  abs(min(row, na.rm = TRUE) - row["nlsch"])
-}
-
-cubic_weight<- function(row){
-  if (is.na(row["cubicd"])){
+weight <- function(row, model_delta, comparing){
+  if (is.na(row[model_delta])){
     0
-  } else{
-    exp(-.5*row["cubicd"])/sum(exp(-.5*row[c("cubicd", "flschd", "nhschd",
-                                             "nlschd")]), na.rm = TRUE)
+  } else {
+    exp(-.5*row[model_delta])/sum(exp(-.5*row[comparing]), na.rm = TRUE)
   }
 }
 
-flsch_weight <- function(row){
-  if (is.na(row["flschd"])){
-    0
-  } else{
-    exp(-.5*row["flschd"])/sum(exp(-.5*row[c("cubicd", "flschd", "nhschd",
-                                             "nlschd")]), na.rm = TRUE)
+compare <- function(models){
+
+  DFaic    <- paste0(models, "DF$nlaic")
+  DFdelta  <- paste0(models, "_delta")
+  DFweight <- paste0(models, "_weight")
+  DFpass   <- paste0(models, "_pass")
+
+
+  aicDF <- data.frame("NewID" = eval(parse(text = (paste0(models[1], "DF$NewID")))))
+
+  for (i in 1:length(models)){
+    aicDF <- cbind(aicDF, eval(parse(text = DFaic[i])))
   }
+
+  colnames(aicDF) <- c("NewID", models)
+
+  for (i in 1:length(models)){
+    aicDF[DFdelta[i]] <- apply(aicDF, 1, delta, model = mdls[i])
+  }
+
+  for (i in 1:length(models)){
+    aicDF[DFweight[i]] <- apply(aicDF, 1, weight, model_delta = DFdelta[i],
+                                comparing = DFdelta)
+  }
+
+  for (i in 1:length(models)){
+    aicDF[DFpass[i]] <- eval(parse(text = (paste0("aicDF$", DFdelta[i])))) <= 2
+  }
+
+  cat("Based off of delta when < 2 from minimum aic means models are comparable\n\n")
+
+  for (i in 1: length(models)){
+    passed <- sum(eval(parse(text = (paste0("aicDF$", DFpass[i])))), na.rm = TRUE)
+
+    cat(paste0(models[i], ": ", passed, " (", round((passed/nrow(aicDF))*100, 2),
+               "%) curves best or comparable to best\n"))
+  }
+
+  pltDF <- data.frame(NewID  = aicDF$NewID, model = mdls[1],
+                      aic    = eval(parse(text = DFaic[1])),
+                      delta  = eval(parse(text = paste0("aicDF$",DFdelta[1]))),
+                      weight = eval(parse(text = paste0("aicDF$",DFweight[1]))))
+
+  for(i in 2:length(mdls)){
+    pltDF <- rbind.data.frame(pltDF,
+                   data.frame(NewID  = aicDF$NewID, model = models[i],
+                              aic    = eval(parse(text = DFaic[1])),
+                              delta  = eval(parse(text = paste0("aicDF$",DFdelta[i]))),
+                              weight = eval(parse(text = paste0("aicDF$",DFweight[i])))))
+  }
+
+  plt <- ggplot(data = pltDF, aes(y = weight, x = model))
+  plt <- plt + geom_boxplot(outlier.shape=NA)
+  plt <- plt + geom_jitter(position=position_jitter(width=.3, height=0), alpha = 0.3, cex = .5)
+  plt <- plt + stat_summary(fun.y=mean, geom="point", shape=18, size=5, show.legend = FALSE)
+  plt <- plt + theme_classic()
+  print(plt)
+
+  fit <- aov(weight ~ model, data = pltDF)
+
+  cat("\n\n\nNow looking at weighted aic\n\n")
+  print(TukeyHSD(fit))
+
+  return(aicDF)
 }
 
-nhsch_weight <- function(row){
-  if (is.na(row["nhschd"])){
-     0
-  } else{
-    exp(-.5*row["nhschd"])/sum(exp(-.5*row[c("cubicd", "flschd", "nhschd",
-                                             "nlschd")]), na.rm = TRUE)
-  }
-}
-
-nlsch_weight <- function(row){
-  if (is.na(row["nlschd"])){
-    0
-  } else{
-    exp(-.5*row["nlschd"])/sum(exp(-.5*row[c("cubicd", "flschd", "nhschd",
-                                             "nlschd")]), na.rm = TRUE)
-  }
-
-
-aicdf$cubicd <- apply(aicdf, 1, cubic_delta)
-aicdf$flschd <- apply(aicdf, 1, flsch_delta)
-aicdf$nhschd <- apply(aicdf, 1, nhsch_delta)
-aicdf$nlschd <- apply(aicdf, 1, nlsch_delta)
-
-aicdf$cubicw <- apply(aicdf, 1, cubic_weight)
-aicdf$flschw <- apply(aicdf, 1, flsch_weight)
-aicdf$nhschw <- apply(aicdf, 1, nhsch_weight)
-aicdf$nlschw <- apply(aicdf, 1, nlsch_weight)
-
 ###############################################################################
-# gather (try with tidyr if you get time!)
+# run the stuff!
 ###############################################################################
 
-df <- rbind.data.frame(
-      cbind.data.frame(NewID = aicdf$NewID, model = "cubic", aic = aicdf$cubic,
-                       delta = aicdf$cubicd, weight = aicdf$cubicw),
-      cbind.data.frame(NewID = aicdf$NewID, model = "flsch", aic = aicdf$flsch,
-                       delta = aicdf$flschd, weight = aicdf$flschw),
-      cbind.data.frame(NewID = aicdf$NewID, model = "nhsch", aic = aicdf$nhsch,
-                       delta = aicdf$nhschd, weight = aicdf$nhschw),
-      cbind.data.frame(NewID = aicdf$NewID, model = "nlsch", aic = aicdf$nlsch,
-                       delta = aicdf$nlschd, weight = aicdf$nlschw))
+mdls <- c("cubic", "flsch", "nhsch", "nlsch", "arrhn")
+#mdls <- c("flsch", "nhsch", "nlsch", "arrhn")
 
-###############################################################################
-# test plots
-###############################################################################
-
-ggplot(data = df[which(df$weight > 0),], aes(y = weight, x = model)) + geom_boxplot()
-
-###############################################################################
-#
-###############################################################################
-
-mod <- lm(weight ~ model, data = df)
-
-summary(mod)
-anova(mod)
-confint(mod)
-
-
-
+aicDF <- compare(mdls)
